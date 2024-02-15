1b8cf61b7126ea1a733b1bd8006483e67b4e7506
enoch <eenkiga@gmail.com>
2024-02-15 17:37:19 +0300

    bugfix

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,26 +1,32 @@
-import { useState } from 'react'
+import { useState } from "react";
 import {
   View,
   Text,
   TouchableOpacity,
   FlatList,
-  ActivityIndicator
-} from 'react-native'
+  ActivityIndicator,
+} from "react-native";
 
-import { useRouter } from 'expo-router'
+import { useRouter } from "expo-router";
 
-import { COLORS, SIZES } from '../../../constants'
-import PopularJobCard from '../../common/cards/popular/PopularJobCard'
-import styles from './popularjobs.style';
-import useFetch from '../../../hook/useFetch'
+import { COLORS, SIZES } from "../../../constants";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import styles from "./popularjobs.style";
+import useFetch from "../../../hook/useFetch";
 
 const Popularjobs = () => {
   const router = useRouter();
-  const {data, isLoading, error} = useFetch('search', {
-    query: 'React developer',
-    num_pages: 1
-  })
-  
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React developer",
+    num_pages: 1,
+  });
+
+  const [selectedJob, setSelectedJob] = useState();
+
+  const handleCardPress = (item) => {
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id);
+  };
 
   return (
     <View style={styles.container}>
@@ -42,15 +48,18 @@ const Popularjobs = () => {
             renderItem={({ item }) => (
               <PopularJobCard
                 item={item}
-                />
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
+              />
             )}
-            keyExtractor={item => item?.job_id}
+            keyExtractor={(item) => item?.job_id}
             contentContainerStyle={{ columnGap: SIZES.medium }}
-            horizontal />
+            horizontal
+          />
         )}
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default Popularjobs
+export default Popularjobs;
